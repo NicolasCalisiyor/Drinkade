82a3d5dbfbb5151b33e1b47176ef90f3f9e9ec78
Jorazon <oskari.pahkala@gmail.com>
2021-09-24 12:08:46 +0300

    bossman 2

--- juomapeli/src/main/java/com/otp1r16/ExampleScene.java
+++ juomapeli/src/main/java/com/otp1r16/ExampleScene.java
@@ -1,4 +1,5 @@
 package com.otp1r16;
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -7,24 +8,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Yksinkertainen esimerkki JavaFX ohjelmasta
- * @author vesal
- * @version 4.3.2016
- */
-public class App extends Application {
-    @Override
+public class ExampleScene extends Application {
+	@Override
     public void start(Stage primaryStage) throws IOException {
-    	Parent root = FXMLLoader.load(getClass().getResource("Aloitusnäyttö.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("AloitusnÃ¤yttÃ¶.fxml"));
 
         primaryStage.setTitle("FXML Welcome");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
-    /** @param args  ei käytössä  */
-    public static void main(String[] args) {
-        launch(args);
-    }
+	
+	public void launchScene(String[] args) {
+		launch(args);
+	}
 }
