1b0463398515cd220b075adecb79f929176b82f9
Jimi Hjelt <jjjhjelt@gmail.com>
2021-09-24 14:14:01 +0300

    mvc-malli

--- src/main/java/model/HitlerScreen.java
+++ src/main/java/model/HitlerScreen.java
@@ -1,23 +1,20 @@
 package model;
+
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Yksinkertainen esimerkki JavaFX ohjelmasta
- * @author vesal
- * @version 4.3.2016
- */
-public class Aloitusnäkymä extends Application {
+public class HitlerScreen extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
     	Parent root = FXMLLoader.load(getClass().getResource("/view/Aloitusnäyttö.fxml"));
 
-        primaryStage.setTitle("FXML Welcome");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Drinkade");
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
