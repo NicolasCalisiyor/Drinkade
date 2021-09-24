075a6056c42a442fcaf97db472d3c3a4323ebe05
Jimi Hjelt <jjjhjelt@gmail.com>
2021-09-24 11:27:36 +0300

    uus versio

---
+++ src/main/java/model/Aloitusnäkymä.java
@@ -0,0 +1,29 @@
+package model;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Yksinkertainen esimerkki JavaFX ohjelmasta
+ * @author vesal
+ * @version 4.3.2016
+ */
+public class Aloitusnäkymä extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("/view/Aloitusnäyttö.fxml"));
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    /** @param args  ei käytössä  */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
